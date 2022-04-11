20899bd739a958a2e22e98ffec448d93ca0057b2
VuongDV6 <duongvuong.fit@gmail.com>
2022-04-11 22:07:46 +0700

    change code v9

--- docker/server.js
+++ docker/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World 2024');
+    res.send('Hello World 2025');
 });
 
 app.listen(PORT, HOST);
